e5810444cd5248359a0ce276f1bbb512d977ebf3
avataratom <38931879+avataratom@users.noreply.github.com>
2024-08-09 21:22:32 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,17 @@
+require("weapp-adapter.js");
+//
+require("libs/laya.core.js");
+//
+require("libs/laya.ui.js");
+//
+require("libs/laya.physics2D.js");
+//
+require("libs/laya.box2D.js");
+//
+require("libs/laya.wxmini.js");
+//
+
+//
+require("js/bundle.js");
+//
+require("js/index.js");
